6ec1aeccd7be4a5ee935129596dfd5b43f32a0c8
Tahsan <tahsanknmas14@gmail.com>
2023-03-04 22:30:06 +0600

    Style Header and footer and create simple banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Banner1 from '../../../assets/images/banner/1.jpg';
+import Banner2 from '../../../assets/images/banner/2.jpg';
+import Banner3 from '../../../assets/images/banner/3.jpg';
+import Banner4 from '../../../assets/images/banner/4.jpg';
+import Banner5 from '../../../assets/images/banner/5.jpg';
+import Banner6 from '../../../assets/images/banner/6.jpg';
+const Banner = () => {
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={Banner1} className="w-full" alt='bannerImage' />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={Banner2} className="w-full" alt='bannerImage' />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={Banner3} className="w-full" alt='bannerImage' />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={Banner4} className="w-full" alt='bannerImage' />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide5" className="carousel-item relative w-full">
+                <img src={Banner5} className="w-full" alt='bannerImage' />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide6" className="carousel-item relative w-full">
+                <img src={Banner6} className="w-full" alt='bannerImage' />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
