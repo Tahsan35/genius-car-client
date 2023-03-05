597b86e3e05f579f422427c72264e95a75e4e623
Tahsan <tahsanknmas14@gmail.com>
2023-03-05 16:17:04 +0600

    firebase error in api key

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,35 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/images/login/login.svg';
 
 const Login = () => {
-    return (
-        <div className="hero w-full">
-            <div className="hero-content flex-col lg:flex-row md:grid-cols-2">
-                <div className="text-center lg:text-left">
 
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 flex-col lg:flex-row md:grid-cols-2">
+                <div className="text-center lg:text-left">
+                    <img src={logo} alt="login svg" srcset="" />
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-10 py-10">
                     <h1 className="text-4xl text-center font-bold">Login now!</h1>
-                    <div className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <input className="btn btn-accent" type="submit" value="Login" />
                         </div>
-                    </div>
+                    </form>
+                    <p className='text-center'>New to Genius Car <Link className='text-orange-600 font-bold' to='/signup'>Sign Up</Link></p>
                 </div>
             </div>
         </div>
---
+++ src/Pages/Login/SignUp/SignUp/Signup.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../../assets/images/login/login.svg';
+
+const Signup = () => {
+
+    const handleSignUp = event => {
+        event.preventDefault();
+    }
+
+    return (
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 flex-col lg:flex-row md:grid-cols-2">
+                <div className="text-center lg:text-left">
+                    <img src={logo} alt="login svg" srcset="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-10 py-10">
+                    <h1 className="text-4xl text-center font-bold">Sign Up</h1>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input type="text" name='password' placeholder="Password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-accent" type="submit" value="sign up" />
+                        </div>
+                    </form>
+                    <p className='text-center'>Already have an account? <Link className='text-orange-600 font-bold' to='/login'>Log in</Link></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
