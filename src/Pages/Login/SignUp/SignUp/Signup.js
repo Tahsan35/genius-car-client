ae8d811a7b902e042024b0009e3261f004a7da9b
Tahsan <tahsanknmas14@gmail.com>
2023-03-07 13:13:59 +0600

    module 67

--- src/Pages/Login/SignUp/SignUp/Signup.js
+++ src/Pages/Login/SignUp/SignUp/Signup.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../assets/images/login/login.svg';
+import { AuthContext } from '../../../../contexts/AuthProvider/AuthProvider';
 
 const Signup = () => {
+    const { createUser } = useContext(AuthContext);
 
     const handleSignUp = event => {
         event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(err => console.error(err));
     }
 
     return (
         <div className="hero w-full my-20">
             <div className="hero-content grid gap-20 flex-col lg:flex-row md:grid-cols-2">
                 <div className="text-center lg:text-left">
-                    <img src={logo} alt="login svg" srcset="" />
+                    <img className='w-3/4' src={logo} alt="login svg" srcset="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-10 py-10">
                     <h1 className="text-4xl text-center font-bold">Sign Up</h1>
